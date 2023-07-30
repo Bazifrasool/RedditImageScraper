9a22fd9b54977f07cfc67f48031feaa82e367484
Christian Clauss <cclauss@me.com>
2023-07-30 17:32:47 +0200

    ruff --select=I --fix run.py

--- run.py
+++ run.py
@@ -4,27 +4,29 @@ Created on Fri Sep 11 18:31:26 2020
 
 @author: Bazif
 """
-from redditdownloader import download_helper
-from flask import Flask, request, render_template
-import time
 import os
-def initiate_engine(path,speed=100,subreddit="wallpapers"):
+import threading
+import time
+import webbrowser
 
+from flask import Flask, render_template, request
+from redditdownloader import download_helper
+
+
+def initiate_engine(path,speed=100,subreddit="wallpapers"):
     download_helper(i_speed=speed,subreddit=subreddit)
 
 
-
-from flask import Flask
-import webbrowser
-import threading
 app = Flask(__name__)
 
 
 def start_app():
     app.run()
 
+
 def open_browser():
     webbrowser.open("http://localhost:5000")
+
 
 @app.route("/")
 def main_page():
@@ -42,6 +44,7 @@ def my_form_post():
     t_down = threading.Thread(target=initiate_engine,args=(path,int(speed),subreddit,))
     t_down.start()
     return render_template("pg2.html")
+
 
 if __name__ == '__main__':
         t_browser=threading.Thread(target=open_browser)
